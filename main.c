acb1ebbd963602ddb8273bb2ec1fe895c6964be8
SathishKumar-Sekaran <115685997+SathishKumar-Sekaran@users.noreply.github.com>
2023-01-04 20:42:23 +0530

    Update Warning and tolerance check test cases

--- main.c
+++ main.c
@@ -3,29 +3,33 @@
 #include "Analyze_BMS_range.h"
 #include "BMS_Check.h"
 
-extern int (*fun_ptr_arr[])(float);
+extern int selectLanguage;
+extern int tolerancePercentage;
+
 int main()
 {
+	st_BatterTemp.min = 0;
+	st_BatterTemp.max = 45;
+	st_BatterSoc.min = 20;
+	st_BatterSoc.max = 80;
+	st_BatterChargeRate.min = 0;
+	st_BatterChargeRate.max = 0.8;
 	//Test for batteryIsOk function
 	assert(batteryIsOk(25, 70, 0.7));
 	assert(!batteryIsOk(50, 85, 0));
+	selectLanguage = ENGLISH;
 	assert(!batteryIsOk(30, 50, 0.9));
 	assert(!batteryIsOk(60, 70, 1.0));
 	
-	//Test for batteryTempIsOk function
-	assert(((*fun_ptr_arr[0])(40)));
-	assert(!((*fun_ptr_arr[0])(-20)));
-	assert(!((*fun_ptr_arr[0])(60)));
-	
-	//Test for batterySocIsOk function
-	assert(((*fun_ptr_arr[1])(55)));
-	assert(!((*fun_ptr_arr[1])(10)));
-	assert(!((*fun_ptr_arr[1])(90)));
-	
-	//Test for batterychargeRateIsOk function
-	assert(((*fun_ptr_arr[2])(0.7)));
-	assert(!((*fun_ptr_arr[2])(1.0)));
-	assert(!((*fun_ptr_arr[2])(0.9)));
+	//Test for checkBatteryWarning function
+	assert(!checkBatteryWarning(2,78,0.77));
+	tolerancePercentage = 10;
+	assert(!checkBatteryWarning(20,73,0.6));
+	assert(!checkBatteryWarning(35,67,0.73));
+	selectLanguage = GERMAN;
+	assert(!checkBatteryWarning(42.5,72,0.5));
+	assert(!checkBatteryWarning(43,74,0.72));
+	assert(checkBatteryWarning(40,55,0.5));
 	
 	return 0;
 }
